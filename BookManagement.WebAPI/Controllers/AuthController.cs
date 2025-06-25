088ff9b9d285a5dbe50c08b69a99ff2d195f6d90
SUSHIL THAKUR <mt3028788@gmail.com>
2025-06-25 23:36:43 +0530

    Add authcontroller two function

--- BookManagement.WebAPI/Controllers/AuthController.cs
+++ BookManagement.WebAPI/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using BookManagement.WebAPI.Services.Interfaces;
+﻿using BookManagement.WebAPI.Application.DTOs.Authentication;
+using BookManagement.WebAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,6 +19,57 @@ namespace BookManagement.WebAPI.Controllers
             _mailService = mailService;
         }
 
+        [HttpPost("SignUp")]
+        [AllowAnonymous]
+        public async Task<ActionResult<AuthResult>> SignUpAsync([FromBody] RegisterRequest request)
+        {
+            try
+            {
+                if (request is null)
+                    return BadRequest(new { Message = "Invalid register request" });
+
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var user = await _authService.Register(request);
+
+                if (!string.IsNullOrEmpty(user.Message))
+                    return BadRequest(new { Message = user.Message });
+
+                var token = user.EmailVerificationToken;
+                var confirmationLink = Url.Action("ConfirmEmail", "Auth",
+                    new { email = user.Email, token = token }, Request.Scheme);
+
+                await _mailService.SendEmailConfirmationAsync(user.Email, confirmationLink);
+
+                return Ok(new { Message = "Registration successful. Please check your email for confirmation." });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { Message = e.Message });
+            }
+        }
+
+        [HttpPost("SignIn")]
+        [AllowAnonymous]
+        public async Task<ActionResult<AuthResult>> SignInAsync([FromBody] LoginRequest request)
+        {
+            try
+            {
+                if (request is null)
+                    return BadRequest(new { Message = "Invalid login request" });
+
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var result = await _authService.Login(request);
+                return !string.IsNullOrEmpty(result.Message) ? BadRequest(new { Message = result.Message }) : Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { Message = e.Message });
+            }
+        }
 
     }
 }
